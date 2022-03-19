62efbf34144beade021024b422a4aeb05695ea95
Daniel Popp <dpopp783@gmail.com>
2022-03-19 02:03:02 -0400

    improve tikz_code()

--- TikzNode.py
+++ TikzNode.py
@@ -1,19 +1,24 @@
 from typing import List
 import Point
+from collections import defaultdict
+import NodeType
 
 class TikzNode:
 
-    node_type: str
+    node_type: NodeType
     params: dict[str,str]
-    label: str
-    location: Point
+    name:str = ""
+    label: str = ""
+    location: Point = None
 
-    def __init__(self, nt: str, *params: List[List[str]]):
+    def __init__(self, nt: NodeType, name:str, params: List[List[str]], location: Point):
         self.node_type = nt
+        self.name = name
+        self.location = location
 
-        self.params = dict()
+        self.params = defaultdict()
         for keyword, value in params:
-            params[keyword] = value
+            self.params[keyword] = value
 
     def params_to_string(self) -> str:
         ret: str = ""
@@ -21,13 +26,21 @@ class TikzNode:
         for keyword, value in self.params.items():
             ret += f", {keyword}={value}"
 
-        return ret[2:]
+        return ret
 
     def tikz_code(self) -> str:
-        tikz_code: str = f"\\node[{self.node_type}, {self.params_to_string()}]"
+        tikz_code: str = f"\\node[{self.node_type}"
+
+        if self.params:
+            tikz_code += f"{self.params_to_string()}"
+
+        tikz_code += "]"
+
+        if self.name:
+            tikz_code += f" ({self.name})"
 
         if self.location != None:
-            tikz_code += f" at ({self.location.x,self.location.y})"
+            tikz_code += f" at {self.location.x,self.location.y}"
 
         if self.label:
             tikz_code += f" \u007b{self.label}\u007d"
