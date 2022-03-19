0fbbd9099d44d31cad00db51519dedad2580c95c
Daniel Popp <dpopp783@gmail.com>
2022-03-19 02:03:37 -0400

    add fields

--- TikzPicture.py
+++ TikzPicture.py
@@ -1,2 +1,18 @@
+from typing import List
+
+from NodeType import NodeType
+from TikzNode import TikzNode
+from Point import Point
+
 class TikzPicture:
-    pass
+
+    params: List[List[str]] = []
+    node_types: List[NodeType] = []
+    nodes: List[TikzNode] = []
+
+    def __init__(self):
+        pass
+
+
+
+
