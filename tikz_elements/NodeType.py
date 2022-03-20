7bca5c12b853ce6ae26873b70c3b49f4eb69dc6e
Daniel Popp <dpopp783@gmail.com>
2022-03-19 22:13:43 -0400

    add shape and min_size fields

--- tikz_elements/NodeType.py
+++ tikz_elements/NodeType.py
@@ -4,12 +4,17 @@ class NodeType:
 
     name: str
     params: List[str] = []
+    shape: str
+    min_size: float
 
 
-    def __init__(self, name:str, shape:str, **kwargs):
+    def __init__(self, name: str, min_size: float, shape: str, **kwargs):
         self.name = name
         self.params.append("draw")
         self.params.append(shape)
+        self.shape = shape
+        self.min_size = min_size
+        self.params.append(f"minimum size={self.min_size} cm")
 
         for kw, val in kwargs.items():
             self.params.append(f"{kw}={val}")
