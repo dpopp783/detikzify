bf9042738ccd065e5d64bf22bce8d285515101a2
Daniel Popp <dpopp783@gmail.com>
2022-03-20 03:11:56 -0400

    confine node to definable bounds

--- gui/GuiDragDropNode.py
+++ gui/GuiDragDropNode.py
@@ -2,31 +2,48 @@ import pygame
 
 
 class Node:
-    def __init__(self, location):
-        self.rectangle = pygame.rect.Rect(176, 134, 17, 17)
+    def __init__(self, location, bounds):
+        self.rect = pygame.rect.Rect(location)
         # stores whether the node is being dragged
         self.dragging = False
         self.offset_x = 0
         self.offset_y = 0
+        self.bounds = pygame.rect.Rect(bounds)
 
     def get_dragging(self):
         return self.dragging
 
     def get_node(self):
-        return self.rectangle
+        return self.rect
 
     # figures out where the mouse is relative to the rectangle center
     def get_mouse_orientation(self, event):
         self.set_dragging(True)
         mouse_x, mouse_y = event.pos
-        self.offset_x = self.rectangle.x - mouse_x
-        self.offset_y = self.rectangle.y - mouse_y
+        self.offset_x = self.rect.x - mouse_x
+        self.offset_y = self.rect.y - mouse_y
+
+    def move_to_cursor(self, event):
+        if self.bounds.collidepoint(event.pos):
+            self.update_location(event)
 
     # updates the location to the new event location
     def update_location(self, event):
         mouse_x, mouse_y = event.pos
-        self.rectangle.x = mouse_x + self.offset_x
-        self.rectangle.y = mouse_y + self.offset_y
+        self.rect.x = mouse_x + self.offset_x
+        self.rect.y = mouse_y + self.offset_y
+        self.constrict_to_bounds()
+
+    def constrict_to_bounds(self):
+        if self.rect.left < self.bounds.left:
+            self.rect.left = self.bounds.left
+        elif self.rect.right > self.bounds.right:
+            self.rect.right = self.bounds.right
+
+        if self.rect.top < self.bounds.top:
+            self.rect.top = self.bounds.top
+        elif self.rect.bottom > self.bounds.bottom:
+            self.rect.bottom = self.bounds.bottom
 
     def set_dragging(self, mode):
         self.dragging = mode
@@ -46,7 +63,8 @@ def main():
 
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Tracking System")
-    rectangle = Node([176, 134, 17, 17])
+    bounds = [0, 0, screen_width, screen_height]
+    rectangle = Node([176, 134, 25, 25], bounds)
     clock = pygame.time.Clock()
 
     running = True
@@ -72,11 +90,11 @@ def main():
             # keeps the rectangle at the mouse location
             elif event.type == pygame.MOUSEMOTION:
                 if rectangle.get_dragging():
-                    rectangle.update_location(event)
+                    rectangle.move_to_cursor(event)
 
         # updates the window to test in
         screen.fill(white)
-        pygame.draw.rect(screen, red, rectangle.get_node())
+        pygame.draw.rect(screen, black, rectangle.get_node(), 3)
         pygame.display.flip()
         clock.tick(fps)
 
