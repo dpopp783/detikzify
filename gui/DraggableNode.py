f67333661935937a8d1ef906f41b20ff19a4e12f
Daniel Popp <dpopp783@gmail.com>
2022-03-20 04:40:53 -0400

    node shape is definable and drawable

--- gui/DraggableNode.py
+++ gui/DraggableNode.py
@@ -1,14 +1,16 @@
 import pygame
+from typing import List, Callable
 
 
 class DraggableNode:
-    def __init__(self, location, bounds):
-        self.rect = pygame.rect.Rect(location)
+    def __init__(self, node_rect: List[int], surface: pygame.surface.Surface, shape: str):
+        self.rect = pygame.rect.Rect(node_rect)
         # stores whether the node is being dragged
         self.dragging = False
         self.offset_x = 0
         self.offset_y = 0
-        self.bounds = pygame.rect.Rect(bounds)
+        self.surface = surface
+        self.shape = shape
 
     def get_dragging(self):
         return self.dragging
@@ -23,7 +25,7 @@ class DraggableNode:
         self.offset_y = self.rect.y - mouse_y
 
     def move_to_cursor(self, event):
-        if self.bounds.collidepoint(event.pos):
+        if self.surface.get_rect().collidepoint(event.pos):
             self.update_location(event)
 
     # updates the location to the new event location
@@ -31,18 +33,19 @@ class DraggableNode:
         mouse_x, mouse_y = event.pos
         self.rect.x = mouse_x + self.offset_x
         self.rect.y = mouse_y + self.offset_y
-        self.constrict_to_bounds()
+        self.constrict_to_surface()
 
-    def constrict_to_bounds(self):
-        if self.rect.left < self.bounds.left:
-            self.rect.left = self.bounds.left
-        elif self.rect.right > self.bounds.right:
-            self.rect.right = self.bounds.right
+    def constrict_to_surface(self):
+        surface_rect = self.surface.get_rect()
+        if self.rect.left < surface_rect.left:
+            self.rect.left = surface_rect.left
+        elif self.rect.right > surface_rect.right:
+            self.rect.right = surface_rect.right
 
-        if self.rect.top < self.bounds.top:
-            self.rect.top = self.bounds.top
-        elif self.rect.bottom > self.bounds.bottom:
-            self.rect.bottom = self.bounds.bottom
+        if self.rect.top < surface_rect.top:
+            self.rect.top = surface_rect.top
+        elif self.rect.bottom > surface_rect.bottom:
+            self.rect.bottom = surface_rect.bottom
 
     def set_dragging(self, mode):
         self.dragging = mode
@@ -50,6 +53,13 @@ class DraggableNode:
     def enter_dragging_mode(self, event):
         self.set_dragging(True)
         self.get_mouse_offset(event)
+
+    def draw_func(self) -> Callable:
+        if self.shape == "rect":
+            return pygame.draw.rect
+        elif self.shape == "circle":
+            return pygame.draw.ellipse
+
 
 def main():
     screen_width = 430
@@ -66,9 +76,9 @@ def main():
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Tracking System")
 
-    bounds = [0, 0, screen_width, screen_height]
-    sqnode = DraggableNode([100, 100, 25, 25], bounds)
-    cnode = DraggableNode([200, 100, 25, 25], bounds)
+    canvas = pygame.surface.Surface((screen_width, screen_height))
+    sqnode = DraggableNode([100, 100, 50, 50], canvas, "rect")
+    cnode = DraggableNode([200, 100, 26, 26], canvas, "circle")
 
     nodes = [cnode, sqnode]
     active_node = None
@@ -91,7 +101,8 @@ def main():
                         if node.get_node().collidepoint(event.pos):
                             active_node = node
 
-                    active_node.enter_dragging_mode(event)
+                    if active_node:
+                        active_node.enter_dragging_mode(event)
 
             # checks whether done dragging
             elif event.type == pygame.MOUSEBUTTONUP:
@@ -108,8 +119,11 @@ def main():
 
         # updates the window to test in
         screen.fill(white)
-        pygame.draw.rect(screen, black, sqnode.get_node(), 3)
-        pygame.draw.circle(screen, black, cnode.get_node().center, cnode.get_node().width // 2, 3)
+        canvas.fill(white)
+        for node in nodes:
+            draw_func = node.draw_func()
+            draw_func(node.surface, pygame.color.Color("#000000"), node.rect, 2)
+        screen.blit(canvas, (0,0))
         pygame.display.flip()
         clock.tick(fps)
 
